f80a655e1d1e9cc66656085ec3c065052d045c55
Zucaritas46 <frosty46@protonmail.com>
2024-07-07 22:48:54 -0700

    added test for images

--- tests/test_image_display.py
+++ tests/test_image_display.py
@@ -1,7 +1,10 @@
 import unittest
 import torch
+import numpy as np
+from PIL import Image
+import tqdm as tqdm
 import matplotlib.pyplot as plt
-from src.Carcinoma import DataFrame
+from src.Carcinoma import DataFrame, DCMReader
 
 
 class TestImageDisplay(unittest.TestCase):
@@ -17,6 +20,24 @@ class TestImageDisplay(unittest.TestCase):
             plt.close('all')  # Close the plot to avoid display during testing
         except Exception as e:
             self.fail(f"show_grid() raised an exception: {e}")
+
+    def test_image(self):
+        # Load and visualize images
+        data_frame = self.data_frame
+        dcm = DCMReader(data_frame)
+        images = dcm.load_images()
+        PIL_images = []
+        for img, target in tqdm(images):
+            im = Image.fromarray(np.uint16(img)).convert("L")
+            PIL_images.append([im, target])
+
+        fig, axs = plt.subplots(2, 5, figsize=(20, 8))
+        for ax, (img, target) in zip(axs.flatten(), PIL_images[:10]):
+            ax.imshow(img, cmap="gray")
+            ax.set_title(f"Target: {target}")
+            ax.axis("off")
+        plt.tight_layout()
+        plt.show()
 
 
 if __name__ == '__main__':
